1b2cc04d79e3c807ed09b9407901b18f2dbdb1fe
Att1Tude <101552647+Att1Tudee@users.noreply.github.com>
2022-07-23 11:48:08 +0300

    Update server.js
    Added path extension

--- motd/mern/server/server.js
+++ motd/mern/server/server.js
@@ -1,3 +1,4 @@
+const path = require(“path”);
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -5,6 +6,7 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 1001;
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, “client/build”)))
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
